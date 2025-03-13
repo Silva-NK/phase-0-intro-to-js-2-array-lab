97742c9e0253b8e591e823e7bd2f3053c6a8a8c0
Silva-NK <kiagonsuzanne@gmail.com>
2025-03-13 13:05:09 +0300

    Adds array cats and performs 4 destructive and 4 non-destructive functions on the array.

--- index.js
+++ index.js
@@ -1,1 +1,85 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+// beforeEach(function () {
+//     cats.length = 0;
+
+//     cats.push('Milo', 'Otis', 'Garfield');
+//   });
+
+
+
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+    
+    return cats;
+}
+
+
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+    
+    return cats;
+}
+
+
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    
+    return cats;
+}
+
+
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    
+    return cats;
+}
+
+
+
+function appendCat(){
+    const copyofCats = cats.slice();
+
+    copyofCats.push("Broom");
+
+    return copyofCats;
+}
+
+
+
+function prependCat(){
+    const copyofCats = cats.slice();
+    
+    copyofCats.unshift("Arnold");
+    
+    return copyofCats;
+}
+
+
+
+function removeLastCat(){
+  const copyofCats = cats.slice();
+  
+  copyofCats.pop();
+  
+  return copyofCats;
+}
+
+
+
+function removeFirstCat(){
+  const copyofCats = cats.slice();
+  
+  copyofCats.shift();
+  
+  return copyofCats;
+}
+
+
+
+
+
+  
